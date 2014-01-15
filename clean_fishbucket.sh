33d4de0b586b027f9a6c087888174cee70f85b1d
billdc <billdc1221@gmail.com>
2014-01-15 17:06:45 +0800

    finish

--- clean_fishbucket.sh
+++ clean_fishbucket.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
-DIRECTORY='/home/applog/test'
-if [ "`ls -A $DIRECTORY`" = "" ];then
-    echo "no file"
+SPLUNK=/opt/splunk/bin/splunk
+while true;do
+    TIME=`date +%H%M%S`
+    LOG_DIR='/home/applog/test'
+    if [ $TIME -le 170000 ];then
+        if [ "`ls -A $LOG_DIR`" = "" ];then
+            echo "stop splunk"
+            date +%H%M%S
+            #splunk stop
+            #splunk clean all
+            break
+        fi
+    else
+        break
+    fi
+    sleep 5
+done
 
-
-
-else 
-    echo "not empty"
-fi
+while true;do
+    if [ $DATE -gt 140000 ];then
+        echo "start splunk"
+        exit
+    fi
+    sleep 10
+done
