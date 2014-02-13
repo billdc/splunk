6c0efa5d7937304c599ef969ef9c3db256389751
billdc <billdc1221@gmail.com>
2014-02-14 00:02:00 +0800

    ssh

---
+++ config_files/set_ssh.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+plink -N -v -P 22 user@hostname -D 127.0.0.1:7070 -pw password
+
--- config_files/ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-plink -N -v -P 22 cdcdssh@s20.flyssh.net -D 127.0.0.1:7070 -pw i9yI838Eiq
-
